a6262fd9e723739a805cc70c8001c28310ed59a0
John <johntdaniel18@gmail.com>
2025-12-24 19:21:12 +0700

    JV LMS

--- firebase.ts
+++ firebase.ts
@@ -3,13 +3,13 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCbxihn2gZvFqgV7K71dCU8tyslDXxC6qU",
-  authDomain: "ielts-lms-e2437.firebaseapp.com",
-  projectId: "ielts-lms-e2437",
-  storageBucket: "ielts-lms-e2437.firebasestorage.app",
-  messagingSenderId: "893003494356",
-  appId: "1:893003494356:web:cf99139a8b806cdb195c41",
-  measurementId: "G-FGEME93L6B"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
